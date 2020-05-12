cdbd5487db4948d97e9a4c48b54bca7e0b837b36
Gabriel <gabriel.moura2009@hotmial.com>
2020-05-11 21:51:28 -0300

    criando botao e estilo

--- content.js
+++ content.js
@@ -1,1 +1,12 @@
-console.log("Minha extensão já está rodando")
+const interval = setInterval(()=>{
+    const header = document.querySelector("._3auIg")
+    if(header){
+        console.log(header)
+        clearInterval(interval)
+    
+        const button = document.createElement("button")
+        button.innerHTML = "2x"
+        button.classList.add("twoTimesButton")
+        header.appendChild(button)
+}
+},1000)
