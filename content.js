2284010c71d7d0c94e9e6de51370bd9c2d5efed7
Gabriel <gabriel.moura2009@hotmial.com>
2020-05-11 22:05:45 -0300

    adicionando a funcionalidade para acelerar

--- content.js
+++ content.js
@@ -7,6 +7,14 @@ const interval = setInterval(()=>{
         const button = document.createElement("button")
         button.innerHTML = "2x"
         button.classList.add("twoTimesButton")
+
+        button.addEventListener("click",()=>{
+            const audios = document.querySelectorAll("audio")
+            audios.forEach((audio)=>{
+                audio.playbackRate = 2;
+            })
+        })
+
         header.appendChild(button)
 }
 },1000)
