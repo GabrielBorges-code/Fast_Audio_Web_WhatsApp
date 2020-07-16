c24e8bca19f207fa509f52bba3717c3a67a74f8b
Gabriel <gabriel.moura2009@hotmial.com>
2020-07-16 02:44:26 -0300

    update and add features

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
-# audioFast
- Essa extensão tem o objetivo de acelerar os audios do WhatsApp em 2x. 
- Esse inspirado no projeto do Programador Br - Tutoriais.
+# Fast Audio 
 
- -----**Instalação**-----
- 
- 1 - ative o modo desenvolvedor no **chrome://extensions/**
- 
- 2 - Seleciona a pasta do *audioFast* e arrasta até a pagina de extensões **chrome://extensions/**
+ Essa extensão tem o objetivo de acelerar os audios do WhatsApp no navegador. 
 
- ----- **Download via GitHub** -----
+ Essa extensão foi feita e inspirada no video do Programador Br - Tutoriais.
+
+ Ela adiciona as velocidades 1x, 1.5x e 2x de velocidade    
+
+![imgFastAudio](https://user-images.githubusercontent.com/63865025/87630016-8ac52e00-c70a-11ea-815a-29a30fa023c3.png)
+
+ ## **Instalação**
  
- se você estiver usando o GitHub basta baixar o arquivo como zip, depois descompacta o arquivo
-e faz os procedimentos escritos em instalação
+ 1 - Ative o modo desenvolvedor no *chrome://extensions/*
+ 
+ 2 - Seleciona a pasta do *audioFast* e arrasta até a pagina de extensões *chrome://extensions/*
+
+ 3 - Atualize a página do WhatsApp
+
--- content.js
+++ content.js
@@ -1,20 +1,49 @@
 const interval = setInterval(()=>{
     const header = document.querySelector("._1QUKR")
     if(header){
-        console.log(header)
+        
         clearInterval(interval)
     
+        const buttonN = document.createElement("buttonN")
         const button = document.createElement("button")
-        button.innerHTML = "2x"
-        button.classList.add("twoTimesButton")
+        const button1 = document.createElement("button1")
+        
+        buttonN.innerHTML = "1x"
+        button.innerHTML = "1.5x"
+        button1.innerHTML = "2x"
 
+        //add class in buttons
+        buttonN.classList.add("button")  
+        button1.classList.add("button")
+        button.classList.add("button")
+
+        //Normal speed
+        buttonN.addEventListener("click",()=>{
+            const audios = document.querySelectorAll("audio")
+            audios.forEach((audio)=>{
+                audio.playbackRate = 1;
+            })
+        })
+
+        //Speed 1.5
         button.addEventListener("click",()=>{
+            const audios = document.querySelectorAll("audio")
+            audios.forEach((audio)=>{
+                audio.playbackRate = 1.5;
+            })
+        })
+
+        //Speed 2
+        button1.addEventListener("click",()=>{
             const audios = document.querySelectorAll("audio")
             audios.forEach((audio)=>{
                 audio.playbackRate = 2;
             })
         })
         
+        //creating the button on the page
+        header.appendChild(buttonN)
         header.appendChild(button)
+        header.appendChild(button1)
 }
 },1000)
